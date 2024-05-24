b65c34931a2b427ac10f6068f1e6fe145bce5a62
Ngo Phuoc Loi <loi.ngophuoc@axonactive.com>
2024-05-24 11:31:46 +0700

    Add UserTable component

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
-export default function Customers() {
-  return <p>Customers Page</p>;
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+
+export default async function Customers({
+  searchParams,
+}: {
+  searchParams: { query: string; page: string };
+}) {
+  const customers = await fetchFilteredCustomers(searchParams.query || '');
+
+  return <CustomersTable customers={customers} />;
 }
